f49c3ae5e93aada7039664e7e004ca440570bdc8
Artūras Drūteika <arturas.druteika@gmail.com>
2025-02-02 22:35:44 +0200

    Add policy initialization
    Add theta param
    Add random actions generation

--- learning/mdp_grid_world/dynamic_programming/policy_iteration.py
+++ learning/mdp_grid_world/dynamic_programming/policy_iteration.py
@@ -1,7 +1,7 @@
-from typing import Tuple
+import random
+from typing import Tuple, List
 
-from numpy import np
-
+import numpy as np
 
 class PolicyIteration:
     
@@ -9,19 +9,22 @@ class PolicyIteration:
         self, 
         state_size: Tuple[int, int],
         action_state_size: int,
-        gamma: float
+        gamma: float,
+        theta: float
     ) -> None:
         self.__state_size = state_size
         self.__action_state_size = action_state_size
         self.__gamma = gamma
+        self.__theta = theta
+        self.__actions = [action for action in range(action_state_size)]
         self.__state_values = self.__init_state_values()
-        self.__policy = self.__init_policy()
+        self.__policy = self.__init_random_policy()
         
     def __init_state_values(self):
         return np.zeros(self.__state_size)
     
     def __init_random_policy(self):
-        pass
+        return np.random.choice(self.__actions, size = self.__state_size)
     
     @property
     def state_size(self) -> Tuple[int, int]:
@@ -36,11 +39,20 @@ class PolicyIteration:
         return self.__gamma
     
     @property
+    def theta(self) -> float:
+        return self.__theta
+    
+    @property
+    def actions(self) -> List[int]:
+        return self.__actions
+    
+    @property
     def state_values(self) -> np.ndarray:
         return self.__state_values
     
-    def policy_evaluation(self):
-        pass
+    @property
+    def policy(self):
+        return self.__policy
     
     def evaluate_policy(self):
         pass
