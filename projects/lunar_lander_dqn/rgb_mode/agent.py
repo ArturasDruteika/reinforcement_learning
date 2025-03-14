d2f4bca6859cc5ba35aff0a89fafe1ef00f722da
Artūras Drūteika <arturas.druteika@gmail.com>
2025-03-14 17:39:04 +0200

    Update agent

--- projects/lunar_lander_dqn/rgb_mode/agent.py
+++ projects/lunar_lander_dqn/rgb_mode/agent.py
@@ -1,25 +1,31 @@
 import torch
+from torch import nn
+from torch import optim
 import rootutils
 
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 
-from projects.lunar_lander_dqn.net import LunarLanderConvNet
-from projects.lunar_lander_dqn.replay_memory import ReplayMemory
+from projects.lunar_lander_dqn.rgb_mode.net import LunarLanderCNN
+from projects.lunar_lander_dqn.rgb_mode.replay_memory import ReplayMemory
 
 
 class LunarLanderDQNAgent:
     
     def __init__(
-        self, 
+        self,
+        action_state_size,
         learning_rate = 1e-5, 
         gamma = 0.99, 
         epsilon = 1.0, 
         epsilon_decay = 0.999, 
-        min_epsilon = 1e-5, 
-        memory_size = 1024,
-        shuffle = False,
-        batch_size = 32
+        min_epsilon = 1e-4, 
+        memory_size = 100_000,
+        shuffle = True,
+        batch_size = 128,
+        sync_target_every: int = 10_000
+        
     ):
+        self.__action_state_size = action_state_size
         self.__learning_rate = learning_rate
         self.__gamma = gamma
         self.__epsilon = epsilon
@@ -28,10 +34,21 @@ class LunarLanderDQNAgent:
         self.__memory_size = memory_size
         self.__shuffle = shuffle
         self.__batch_size = batch_size
+        self.__sync_target_every: int = sync_target_every
 
-        self.__model = LunarLanderConvNet()
-        self.__target_model = LunarLanderConvNet()
-        self.__replay_memory = ReplayMemory(self.__memory_size, self.__batch_size, self.__shuffle)
+        self.__model = LunarLanderCNN(self.__action_state_size)
+        self.__target_model = LunarLanderCNN(self.__action_state_size)
+        self.__target_model.load_state_dict(self.__model.state_dict())
+        self.__target_model.eval()
+        
+        self.__optimizer = optim.Adam(self.__model.parameters(), lr = self.__learning_rate)
+        self.__criterion = nn.SmoothL1Loss()
+        
+        self.__replay_memory = ReplayMemory(self.__memory_size, self.__shuffle)
+        
+    @property
+    def action_state_size(self):
+        return self.__action_state_size
         
     @property
     def learning_rate(self):
@@ -44,6 +61,10 @@ class LunarLanderDQNAgent:
     @property
     def epsilon(self):
         return self.__epsilon
+    
+    @epsilon.setter
+    def epsilon(self, value):
+        self.__epsilon = value
     
     @property
     def epsilon_decay(self):
@@ -66,12 +87,24 @@ class LunarLanderDQNAgent:
         return self.__batch_size
     
     @property
+    def sync_target_every(self):
+        return self.__sync_target_every
+    
+    @property
     def model(self):
         return self.__model
     
     @property
     def target_model(self):
         return self.__target_model
+    
+    @property
+    def optimizer(self):
+        return self.__optimizer
+    
+    @property
+    def criterion(self):
+        return self.__criterion
     
     @property
     def replay_memory(self):
@@ -84,6 +117,8 @@ class LunarLanderDQNAgent:
         self._epsilon = max(self._epsilon * self._epsilon_decay, self._min_epsilon)
         
     def choose_action(self, state: torch.tensor) -> int:
+        self.__model.eval()
+        
         with torch.no_grad():
             if torch.rand(1).item() < self.epsilon:
                 return torch.randint(0, 4, (1,)).item()
@@ -114,3 +149,33 @@ class LunarLanderDQNAgent:
         
     def load_target_model(self, filepath: str) -> None:
         self.__target_model.load_model_data(filepath)
+        
+    def learn(self, return_loss = False):
+        if not self.__replay_memory.is_full:
+            return
+        
+        self.__model.train()
+        
+        states, actions, rewards, next_states, dones = self.__replay_memory.sample(
+            batch_size=self.__batch_size,
+            torch_tensor=True
+        )
+        
+        q_values = self.__model(states).gatcher(1, actions.unsqueeze(-1)).squezze(-1)
+        with torch.no_grad():
+            next_q_values = self.__target_model(next_states).max(dim=1).values
+            expected_q_values = rewards + self.__gamma * next_q_values * (1 - dones.float())
+            
+        loss = self.__criterion(q_values, expected_q_values)
+        
+        self.__optimizer.zero_grad()
+        loss.backward()
+        self.__optimizer.step()
+        
+        self.__learning_step += 1
+        
+        if self.__learning_step % self.__sync_target_every == 0:
+            self.update_target_model()
+        
+        if return_loss:
+            return loss
